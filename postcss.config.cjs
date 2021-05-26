590d16ee81110f6d40dcdb4c8cbbb875fde0993c
shenshi <shenshi@shouqianba.com>
2021-05-26 17:25:17 +0800

    feat:  .cjs .mjs .js 区别

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,4 +1,11 @@
 // postcss.config.cjs
+// cjs 即 commonjs  文件格式为 commonjs 语法，配置使用 module.exports = {} 导出
+// mjs 即  ECMAScript modules 的语法，使用 export 导出
+
+/**
+ * babel.config.js: 如果 babel 配置里声明了 type: module，和 babel.config..mjs 行为一样，
+ * 否则和 babel.config.cjs 行为一致，使用 mocule.exports = {} 导出
+ */
 module.exports = {
     plugins: {
         tailwindcss: {},
